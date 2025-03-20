c55142679116f0235b7504cb2bc8cd5e0b8459b4
ghrv <49789814+ghrv@users.noreply.github.com>
2025-03-20 13:51:01 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,30 +9,30 @@ const CONFIG = {
     // Chemin vers le fichier de données
     dataUrl: 'data/poi.json',
     
-    // Types de points d'intérêt et leurs icônes (utilisant les icônes Google Maps par défaut)
+    // Types de points d'intérêt et leurs icônes
     poiTypes: {
         'eglise': {
-            icon: 'https://maps.google.com/mapfiles/ms/icons/purple-dot.png',
+            icon: 'data/images/church.png',
             title: 'Église'
         },
         'menhir': {
-            icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            icon: 'data/images/menhir.png',
             title: 'Menhir'
         },
         'megalithe': {
-            icon: 'https://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+            icon: 'data/images/megalith.png',
             title: 'Mégalithe'
         },
         'moulin': {
-            icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+            icon: 'data/images/mill.png',
             title: 'Moulin à eau'
         },
         'four': {
-            icon: 'https://maps.google.com/mapfiles/ms/icons/orange-dot.png', 
+            icon: 'data/images/oven.png', 
             title: 'Four à pain'
         },
         'autre': {
-            icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+            icon: 'data/images/landmark.png',
             title: 'Point d\'intérêt'
         }
     }
